f87b82fdf8396924472cde1ef372e30e6fd1330d
zhi_glb <zhi_glb@gmail.com>
2025-03-25 23:46:21 +0800

    feat(ts): 利用函數設定是否顯示

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,12 @@ let products = [
   new Product(1, "圖像 Angular 開發入門 第二版", "博碩文化", 500, true, new Date("2024-4-10")),
   new Product(2, "金魚都能懂的 CSS 必學屬性", "博碩文化", 500, true, new Date("2024-4-10")),
 ];
-console.log(products);
 console.table(products);
+
+setProductDisplay(products[0], false);
+
+console.table(products);
+
+function setProductDisplay(product: Product, isShow: boolean) {
+  product.isShow = isShow;
+}
