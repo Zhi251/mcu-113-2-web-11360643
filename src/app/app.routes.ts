3a323cb2cbe5f8a8073af66d8bdcc4ce4f87ab54
zhi_glb <zhi_glb@gmail.com>
2025-04-23 21:10:31 +0800

    feat(router): 切換產品清單與登入頁面實作

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,9 @@
 import { Routes } from '@angular/router';
-import { ProductCardComponent } from './product-card/product-card.component';
+import { ProductPageComponent } from './product-page/product-page.component';
+import { LoginPageComponent } from './login-page/login-page.component';
 
-export const routes: Routes = [{ path: '', component: ProductCardComponent }];
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'products' },
+  { path: 'products', component: ProductPageComponent },
+  { path: 'login', component: LoginPageComponent },
+];
